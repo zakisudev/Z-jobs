0b0f8e3d01b5c32195bd7b42ee8be25f2e9d070d
Zekaria Mohammed <zakisu250@gmail.com>
2023-12-12 23:00:46 +0300

    Refactor server.js to remove unnecessary code and improve static file serving

--- backend/server.js
+++ backend/server.js
@@ -22,18 +22,14 @@ app.use((req, res, next) => {
 // Cookie parser
 app.use(cookieParser());
 
-// make uploads folder static
-const __dirname = path.resolve();
-app.use('/upload', express.static(path.join(__dirname, 'frontend', ' public')));
-
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
-  app.get('*', (req, res) =>
+  app.get('*', (_, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   );
 } else {
-  app.get('/', (req, res) => {
+  app.get('/', (_, res) => {
     res.send('API is running...');
   });
 }
