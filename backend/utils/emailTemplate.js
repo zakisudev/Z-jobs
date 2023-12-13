a7394bc3b57e5e91b31137552f4e4ebc990980c8
Zekaria Mohammed <zakisu250@gmail.com>
2023-12-13 19:56:36 +0300

    Update email template for registration

--- backend/utils/emailTemplate.js
+++ backend/utils/emailTemplate.js
@@ -1,11 +1,13 @@
 const emailTemplate = () => {
   return `
-    <h1>Thank you for registering!</h1>
-    <p>You have successfully registered for Z-jobs.</p>
-    <p>Please click on the link below to verify your email address.</p>
-    <a href=${process.env.VERIFY_EMAIL_ADDRESS}>Verify Email</a>
+    <h1>Thank you for registering!</h1><br/>
+    <p>You have successfully registered for Z-jobs.</p><br/>
+    <p>Please click on the link below to verify your email address.</p><br/>
+    <a href=${process.env.VERIFY_EMAIL_ADDRESS}>Verify Email</a><br/>
+    <br/>
+    <p> p.s. If you did not register for Z-jobs, please ignore this email.</p><br/>
     <p>Thank you!</p>
   `;
 };
 
-export default emailTemplate;
+module.exports = emailTemplate;
